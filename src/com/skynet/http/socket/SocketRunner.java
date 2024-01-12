f465001dcf406055f1dec38ae8d1d923ff9fefc0
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2024-01-12 08:06:26 +0100

    feat: 2.tcpServerSocket

--- src/com/skynet/http/socket/SocketRunner.java
+++ src/com/skynet/http/socket/SocketRunner.java
@@ -2,19 +2,22 @@ package com.skynet.http.socket;
 
 import java.io.*;
 import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class SocketRunner {
     public static void main(String[] args) throws IOException {
-        var inetAddresses = Inet4Address.getByName("google.com");
-        try (Socket socket = new Socket(inetAddresses, 80);
+        var inetAddresses = Inet4Address.getByName("localhost");
+        try (Socket socket = new Socket(inetAddresses, 7777);
              var outputStream = new DataOutputStream(socket.getOutputStream());
-             var inputStream = new DataInputStream(socket.getInputStream())) {
+             var inputStream = new DataInputStream(socket.getInputStream());
+             Scanner scanner = new Scanner(System.in)) {
 
-            outputStream.writeUTF("Hello world!");
-            byte[] response = inputStream.readAllBytes();
-            System.out.println(response.length);
+            while (scanner.hasNextLine()) {
+                String request = scanner.nextLine();
+                outputStream.writeUTF(request);
+                System.out.println("Response from server: " + inputStream.readUTF());
+            }
         }
     }
 }
